5abcc6bcbabd8e610e85710e2786cadf243a82b7
Aaron Aragon <aaron.david.nagore@gmail.com>
2023-04-27 21:52:42 +0200

    feat(structure): First commit with API base.

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const logger = require('./loggerMiddleware')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(logger)
+
+let notes = [
+  {
+    id: 1,
+    content: 'Me tengo que suscribir a @midudev en YouTube y Twich',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Tengo que estudiar las clases del FullStack Bootcamp',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'Repasar los retos de JS de midudev',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "content-type": "application/json" });
+//   response.end(JSON.stringify(notes));
+// });
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find((note) => note.id === id)
+
+  note ? response.json(note) : response.status(404).end()
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter((note) => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+
+  if (!note || !note.content) {
+    return response.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
+
+  const ids = notes.map((note) => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+
+  notes = [...notes, newNote]
+
+  response.response(201).json(newNote)
+})
+
+app.use((request, response) => {
+  response.status(404).json({
+    error: 'Not found'
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
