92c8821d2744a6e7c6b9ae09694982f246d0f99d
Aaron Aragon <aaron.david.nagore@gmail.com>
2023-04-27 22:15:52 +0200

    (fix): Added Heroku config.

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.use((request, response) => {
   })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
