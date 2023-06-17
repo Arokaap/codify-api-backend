64a7642df5f018b2c56827a90919189b8f53c838
Aaron Aragon <aaron.david.nagore@gmail.com>
2023-06-17 10:43:37 +0200

    feat(image user): Added upload to image user.

---
+++ controllers/uploadImageUser.js
@@ -0,0 +1,40 @@
+const Upload = require('../models/Upload')
+const cloudinary = require('../lib/cloudinary')
+const User = require('../models/User')
+
+exports.uploadImageUser = async (req, res, next) => {
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path,
+      {
+        resource_type: 'image',
+        folder: 'images'
+      }
+    )
+
+    const upload = new Upload({
+      name: req.file.originalname,
+      url: result.url,
+      cloudinary_id: result.public_id,
+      description: req.body.description
+    })
+
+    const savedUpload = await upload.save()
+
+    const { idUser } = req.params // id del curso para el cual se subirá la imagen
+
+    const updatedUser = await User.findByIdAndUpdate(
+      idUser,
+      { image: savedUpload.url },
+      { new: true, runValidators: true, context: 'query' }
+    )
+
+    if (updatedUser) {
+      res.status(200).json(updatedUser)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+}
--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 const uploadController = require('../controllers/upload')
 const uploadImageController = require('../controllers/uploadImage')
 const uploadImageLessonController = require('../controllers/uploadImageLesson')
+const uploadImageUserController = require('../controllers/uploadImageUser')
 
 const storage = require('../lib/multer')
 
 router.post('/uploadVideo/:idLesson', storage.single('file'), uploadController.uploadVideo)
 router.post('/uploadImageCourse/:idCourse', storage.single('file'), uploadImageController.uploadImage)
 router.post('/uploadImageLesson/:idLesson', storage.single('file'), uploadImageLessonController.uploadImageLesson)
+router.post('/uploadImageUser/:idUser', storage.single('file'), uploadImageUserController.uploadImageUser)
 
 module.exports = router
