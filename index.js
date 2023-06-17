73ad30181c0abefe00e93fec73bd5e5049ffa86a
Aaron Aragon <aaron.david.nagore@gmail.com>
2023-06-17 19:29:05 +0200

    fix(cors): CORS error try to solve.

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ const routes = require('./routes/uploadRoute')
 const bodyParser = require('body-parser')
 
 app.use(cors())
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization')
+  next()
+})
+
 app.use(express.json())
 app.use('/images', express.static('images'))
 
